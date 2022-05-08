f1d41e42068f54421733b71a637662e6d7e87ab4
Cris Sang <pxsang194@gmail.com>
2022-05-08 11:15:52 +0700

    add news model

---
+++ api/news/controllers/news.js
@@ -0,0 +1,48 @@
+"use strict";
+
+/**
+ * Read the documentation (https://strapi.io/documentation/3.0.0-beta.x/concepts/controllers.html#core-controllers)
+ * to customize this controller
+ */
+const { sanitizeEntity } = require("strapi-utils");
+
+module.exports = {
+  async find(ctx) {
+    const query = { ...ctx.query };
+    const { page = 1, per_page = 6 } = query;
+    delete query.page;
+    delete query.per_page;
+    query._start = (page - 1) * per_page;
+    query._limit = per_page;
+    query.status = "publish";
+
+    let entities;
+
+    entities = await strapi.services["news"].find(query);
+
+    return entities.map((entity) =>
+      sanitizeEntity(entity, { model: strapi.models["news"] })
+    );
+  },
+
+  count(ctx) {
+    const query = { ...ctx.query };
+    query.status = "publish";
+
+    return strapi.services["news"].count(query);
+  },
+
+  async findOne(ctx) {
+    const slug = ctx.params.id;
+
+    let entity = await strapi.services["news"].findOne({ slug });
+    const count = parseInt(entity.count || 0);
+    entity = await strapi.services["news"].update(
+      { slug },
+      { count: count + 1 }
+    );
+    return sanitizeEntity(entity, {
+      model: strapi.models["news"],
+    });
+  },
+};
---
+++ api/news/models/news.js
@@ -0,0 +1,16 @@
+"use strict";
+
+/**
+ * Lifecycle callbacks for the `news` model.
+ */
+const slugify = require("slugify");
+
+module.exports = {
+  beforeSave: async (model, attrs, options) => {
+    if (options.method === "insert" && attrs.title) {
+      model.set("slug", slugify(attrs.title.toLowerCase()));
+    } else if (options.method === "update" && attrs.title) {
+      attrs.slug = slugify(attrs.title.toLowerCase());
+    }
+  },
+};
